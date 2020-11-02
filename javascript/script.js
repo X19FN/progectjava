d7022393c50a575a52262211ce59996018e19e51
Andrew <spam.228@mail.ru>
2020-11-02 21:10:06 +0700

    files and movies

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,15 @@
 "use strict";
 
-const numberOfFilms = prompt ("Сколько фильмов вы уже посмотрели?","");
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// function message(text) {
+//     console.log(text);
+//     let num = 10;
+//
+// }
+// message("Hello");
+// console.log(num);
 
-const a = prompt ("Один из последних просмотренных фильмов?",""),
-      b = prompt ("На сколько оцените его?", ""),
-      c = prompt ("Один из последних просмотренных фильмов?",""),
-      d = prompt ("На сколько оцените его?", "");
+function calc (a,b) {
+    return (a + b);
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
-
-
-
-
+console.log(calc(1.15,2.30));
